33c75f0827be286675f9e7d12b6fc2e8174564f3
Renan Franca <renanfranca.dev@gmail.com>
2023-12-11 15:59:36 -0300

    feat: novas rotas configuradas

--- src/api/NewsService.js
+++ src/api/NewsService.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
-const url = "https://api.spaceflightnewsapi.net/v4/articles/21783/";
+const url = "https://api.spaceflightnewsapi.net/v4/articles/?limit=2";
 
 export class NewsService {
   static getNews() {
     return axios(url);
+  }
+
+  static getNewsDetails(id) {
+    return axios(url + `/${id}`);
+  }
+
+  static searchNews(search) {
+    return axios(url + `/?search=${search}`);
   }
 }
