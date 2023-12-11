332edab90dff45c48b5fd8d0301c76be9ae794f1
Renan Franca <renanfranca.dev@gmail.com>
2023-12-11 15:03:42 -0300

    feat: página home criada

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { NewsService } from "../api/NewsService";
+import Card from "../components/Card";
+
 const Home = () => {
-  return <h1>Flight News</h1>;
+  const [news, setNews] = useState([]);
+
+  const getNews = async () => {
+    const { data } = await NewsService.getNews();
+    console.log(data);
+
+    setNews(data);
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  return (
+    <div className="w-full flex justify-center">
+      <Card data={news} />
+    </div>
+  );
 };
 
 export default Home;
