8b3059d4d4d4121cd0ba9c8efaaa8b76be2c6904
Renan Franca <renanfranca.dev@gmail.com>
2023-12-11 11:42:59 -0300

    chore: estrutura vitte + routerdom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Home from "./views/Home";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./views/Home/Home.jsx";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
