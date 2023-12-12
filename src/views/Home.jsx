6b56d456c659eb616bcece3a6b36d5628928248c
Renan Franca <renanfranca.dev@gmail.com>
2023-12-12 17:07:55 -0300

    feat: atualização dos hooks para limit e offset + dateformat das noticias

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,39 +3,57 @@ import { NewsService } from "../api/NewsService";
 import Card from "../components/Card";
 
 const Home = ({ searchValueProp, sortValueProp }) => {
-  console.log(searchValueProp);
-  console.log(sortValueProp);
+  // console.log(searchValueProp);
+  // console.log(sortValueProp);
   const [news, setNews] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [offset, setOffset] = useState(0);
 
-  const getNews = async () => {
-    const { data } = await NewsService.getNews();
+  const getNews = async (limit, offset) => {
+    console.log("limit", limit);
+    console.log("offset", offset);
+    const { data } = await NewsService.getNews(limit, offset);
     setNews(data.results);
     // console.log(news);
   };
 
   useEffect(() => {
-    getNews();
+    getNews(limit, offset);
   }, []);
 
-  const getNewsSearch = async (searchValue) => {
-    const { data } = await NewsService.searchNews(searchValue);
+  const getNewsSearch = async (searchValueProp) => {
+    const { data } = await NewsService.searchNews(searchValueProp);
     setNews(data.results);
   };
 
   useEffect(() => {
     if (searchValueProp.length > 0) {
-      getNewsSearch();
+      getNewsSearch(searchValueProp);
     }
     if (searchValueProp === "") {
-      getNews();
+      getNews(limit, offset);
     }
   }, [searchValueProp]);
 
+  const handleClick = () => {
+    setOffset((prev) => {
+      return prev + 10;
+    });
+    setLimit((prev) => {
+      return prev + 10;
+    });
+  };
+
+  useEffect(() => {
+    getNews(limit, offset);
+  }, [offset]);
+
   return (
-    <div className="w-full flex flex-col justify-center">
+    <div className="w-full flex flex-col justify-center items-center gap-16">
       {news.map((item, index) => {
         return <Card key={index} data={item} />;
       })}
+      <button onClick={handleClick}>Ver Mais</button>
     </div>
   );
 };
