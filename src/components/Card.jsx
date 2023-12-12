858295452b000e3d5ea0c31683d21149df3c9b9d
Renan Franca <renanfranca.dev@gmail.com>
2023-12-12 17:07:10 -0300

    style: centralização do card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,15 @@
 const Card = ({ data }) => {
+  var date = new Date(data.published_at);
+
+  let day = date.getDate();
+  // day.length > 1 ? day : "0" + day;
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  let dayFormat = day + "/" + month + "/" + year;
+
   return (
-    <div className="flex flex-row gap-4">
+    <div className="flex flex-row gap-4 justify-center items-center">
       <div>
         <img
           src={data.image_url}
@@ -14,15 +23,15 @@ const Card = ({ data }) => {
       <div className="flex flex-col w-[400px] gap-4">
         <h2>{data.title}</h2>
         <div className="flex items-center justify-between mx-2">
-          <small>{data.published_at}</small>
+          <small>{dayFormat}</small>
           <p href={data.url} className="border-2 border-black bg-gray-300 px-1">
             {data.news_site}
           </p>
         </div>
-        <p>{data.summary}</p>
+        <p className="overflow-hidden h-[100px]">{data.summary}...</p>
         <a
           href={data.url}
-          className="border-2 w-[100px] text-center text-white bg-gray-700 py-2"
+          className="border-2 w-[100px] text-center text-white py-2"
         >
           Ver mais
         </a>
